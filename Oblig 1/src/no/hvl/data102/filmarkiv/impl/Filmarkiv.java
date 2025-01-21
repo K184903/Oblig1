09984062391817e8cd6a3864e60cff6e29cfbe2b
H679280 <stjerneblink1999@live.com>
2025-01-21 14:25:29 +0100

    e

--- Oblig 1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -34,7 +34,9 @@ public class Filmarkiv implements FilmarkivADT {
 	public boolean slettFilm(int filmnr) {
 		for (int i = 0; i < filmer.length; i++) {
 			if (filmer[i].getFilmnr() == filmnr) {
-				filmer[i] = new Film();
+				filmer[i] = filmer[nesteLedig-1];
+				filmer[nesteLedig - 1] = new Film();
+				nesteLedig--;
 				return true;
 			}
 		}
