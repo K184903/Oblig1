05a73aa26ecebe635378865b084dbdd957db113c
K184903 <184903@stud.hvl.no>
2025-01-21 14:30:07 +0100

    h

--- Oblig 1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -1,33 +1,36 @@
 package no.hvl.data102.filmarkiv.impl;
 
+import java.util.ArrayList;
+
 import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
 
 public class Filmarkiv implements FilmarkivADT {
-	private Film[] filmer; 
-    private int nesteLedig;
+	private Film[] filmer;
+	private int nesteLedig;
 
-    public Filmarkiv(int antall) {
-        this.filmer = new Film[antall];
-        this.nesteLedig = 0;
-    }
+	public Filmarkiv(int antall) {
+		this.filmer = new Film[antall];
+		this.nesteLedig = 0;
+	}
 
 	@Override
 	public Film finnFilm(int nr) {
-	  for(int i = 0; i<filmer.length;i++) {
-		  if(filmer[i].getFilmnr()==nr) {
-			  return filmer[i];
-		  }
-	  }
+		for (int i = 0; i < filmer.length; i++) {
+			if (filmer[i].getFilmnr() == nr) {
+				return filmer[i];
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public void leggTilFilm(Film nyFilm) {
-		if(filmer.length<nesteLedig) {
+		if (filmer.length > nesteLedig) {
+			filmer[nesteLedig] = nyFilm;
 			nesteLedig++;
+		} else {
+			System.out.println("Tabellen er full");
 		}
-		
-		
 	}
 
 	@Override
@@ -38,8 +41,15 @@ public class Filmarkiv implements FilmarkivADT {
 
 	@Override
 	public Film[] soekTittel(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+
+		ArrayList<Film> resultater = new ArrayList<>();
+
+		for (int i = 0; i < nesteLedig; i++) {
+			if (filmer[i].getFilm().toLowerCase().contains(delstreng)) {
+				resultater.add(filmer[i]);
+			}
+		}
+		return resultater.toArray(new Film[0]);
 	}
 
 	@Override
@@ -50,8 +60,8 @@ public class Filmarkiv implements FilmarkivADT {
 
 	@Override
 	public int antall(Sjanger sjanger) {
-		// TODO Auto-generated method stub
-		return 0;
+		 
+		 return 0;
 	}
 
 	@Override
@@ -59,9 +69,5 @@ public class Filmarkiv implements FilmarkivADT {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
-	
-	
-	
 
 }
