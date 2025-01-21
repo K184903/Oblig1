310b83c0b8f7ed459bed00b5d20b141eadf686ec
H679280 <stjerneblink1999@live.com>
2025-01-21 14:19:41 +0100

    h

--- Oblig 1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -32,7 +32,12 @@ public class Filmarkiv implements FilmarkivADT {
 
 	@Override
 	public boolean slettFilm(int filmnr) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < filmer.length; i++) {
+			if (filmer[i].getFilmnr() == filmnr) {
+				filmer[i] = new Film();
+				return true;
+			}
+		}
 		return false;
 	}
 
--- Oblig 1/src/no/hvl/data102/filmarkiv/kilent/FilmarkivMain.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/kilent/FilmarkivMain.java
@@ -3,8 +3,8 @@ package no.hvl.data102.filmarkiv.kilent;
 public class FilmarkivMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("hei");
 	}
 
 }
