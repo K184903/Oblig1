9ede3c52837db25bcd705ad711cc06a1837a202a
K184903 <184903@stud.hvl.no>
2025-01-23 12:45:18 +0100

    ferdig lagra

--- Oblig 1/src/no/hvl/data102/filmarkiv/kilent/FilmarkivMain.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/kilent/FilmarkivMain.java
@@ -5,7 +5,9 @@ import no.hvl.data102.filmarkiv.impl.Filmarkiv;
 public class FilmarkivMain {
 
 	public static void main(String[] args) {
-		
-	}
+		FilmarkivADT filma = new Filmarkiv(100);
+		Meny meny = new Meny(filma);
+		meny.start();
 
+	}
 }
