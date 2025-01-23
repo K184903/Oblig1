7225ec66ebd98194ca24a6c4d3e4698d6ee4e54d
K184903 <184903@stud.hvl.no>
2025-01-23 12:43:52 +0100

    organisering

--- Oblig 1/src/no/hvl/data102/filmarkiv/kilent/Meny.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/kilent/Meny.java
@@ -1,5 +1,17 @@
 package no.hvl.data102.filmarkiv.kilent;
 
 public class Meny {
+	private Tekstgrensesnitt tekstgr;
+	private FilmarkivADT filmarkiv;
 
+	public Meny(FilmarkivADT filmarkiv) {
+		tekstgr = new Tekstgrensesnitt();
+		this.filmarkiv = filmarkiv;
+	}
+
+	public void start() {
+		// legg inn en del forhåndsdefinerte filmer for å teste metodene
+		// ..
+		// TODO
+	}
 }
