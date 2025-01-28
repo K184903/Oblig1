d3eba6117b3882b8503bdf3d0afd3df7ffca6b2f
K184903 <184903@stud.hvl.no>
2025-01-28 09:15:04 +0100

    Endret sjangermetode

--- Oblig 1/src/no/hvl/data102/filmarkiv/kilent/FilmarkivMain.java
+++ Oblig 1/src/no/hvl/data102/filmarkiv/kilent/FilmarkivMain.java
@@ -5,8 +5,8 @@ import no.hvl.data102.filmarkiv.adt.*;
 public class FilmarkivMain {
 
 	public static void main(String[] args) {
-		FilmarkivADT filma = new Filmarkiv(100);
-		Meny meny = new Meny(filma);
+		FilmarkivADT filmarkiv = new Filmarkiv(100);
+		Meny meny = new Meny(filmarkiv);
 		meny.start();
 
 	}
